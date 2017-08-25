8bbaeac880c6f7019a467aeb05322958da2b8a1b
Kevin Mahoney <kmahoney@theatlantic.com>
2017-08-25 16:45:27 -0400

    set up local server

---
+++ server.js
@@ -0,0 +1,16 @@
+const sitePath = process.argv[2] || '.';
+const port = 4000;
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  // next() with no arguments allows to pretend you didn't handle the route so that something else can pick it up instead.
+  next();
+});
+
+app.use(express.static(__dirname + '/' + sitePath));
+
+app.listen(port, () => {
+  console.log('server running at ' + port);
+});
