06bce414a9d414115becc4531f87706ecfc96c5e
Kevin Mahoney <kjmahoney13@gmail.com>
2017-08-25 00:03:17 -0400

    add language swap functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-//add language swap
+// @TODO
 //add on change
 //need to add translation loop for pinyin
+//es6 interpolation
+//if chinese is input then
 const pinyin = require("pinyin");
 
 const translate = function(e) {
@@ -8,21 +10,32 @@ const translate = function(e) {
   console.log('firing');
 
   let word = document.getElementById('translate-input').value;
+  let fromLanguage = document.getElementById('translate-from-dropdown').value;
+  let languageUrl;
+
+  if (fromLanguage === 'en') {
+    languageUrl = '&lang=en-zh'
+  } else {
+    languageUrl = '&lang=zh-en'
+  }
 
   let APIUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trns"+
   "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
   +word
-  +"&lang="
-  +'en'
-  +"-"
-  +'zh'
+  +languageUrl
+
 
   let request = new Request(APIUrl);
 
   fetch(request).then((response) => {
     return response.json().then(function(json){
-      word = json.text[0];
+      //if user inputs chinese word then pinyin the chinese in input field
       let pronounce = pinyin(word);
+      //if user inputs english, pinyin the chinese json response
+      if (fromLanguage === 'en') {
+          pronounce = pinyin(json.text[0]);
+      }
+
       document.getElementById('translate-output').innerHTML = json.text[0];
       document.getElementById('pronounce-output').innerHTML = pronounce[0][0];
     })
