40aadb1b4ddc04d77c4b31633f52bbe1080e6b69
Kevin Mahoney <kjmahoney13@gmail.com>
2017-10-09 22:05:42 -0400

    basic styling for index page

--- public/js/dynamic-inbox.js
+++ public/js/dynamic-inbox.js
@@ -1,8 +1,13 @@
-const $ = require('zepto');
-
 console.log('working');
 
-const resizeInputBox = () => {
-    $('c-index__input--adjust').attr('size', $('#dynamicInbox').val().length);
-}
-resizeInputBox();
+$('.c-index__input--adjust').each(function(index, obj) {
+  console.log($(this).attr('size'));
+  console.log($(this).val().length);
+  $(this).attr('size', $(this).val().length + 1);
+  console.log($(this).attr('size'));
+});
+
+// const resizeInputBox = () => {
+//     $('.c-index__input--adjust').attr('size', $('.c-index__input--adjust').val().length);
+// }
+// resizeInputBox();
